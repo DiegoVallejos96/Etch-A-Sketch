faaf28267c6950c6a803e978a4ca18b6b697a71c
DiegoVallejos96 <diegopuan96@gmail.com>
2024-02-26 16:17:26 -0300

    grid dimensions right, styles done, project complete

--- script.js
+++ script.js
@@ -1,104 +1,87 @@
 const body = document.body
-const main = document.querySelector(".container")
-const header = document.querySelector("header")
 
+const container = document.getElementById('grid-container');
+const containerSup = document.querySelector('.containerSup')
 
-const divSup = document.createElement('div')
-divSup.setAttribute('class','divSup')
-header.appendChild(divSup)
+const containerTitle = document.createElement('div')
+containerTitle.setAttribute('class','containerTitle')
+containerSup.appendChild(containerTitle)
+const title = document.createElement('h1')
+title.textContent = 'Etch-A-Sketch'
+containerTitle.appendChild(title)
 
+const containerBtn = document.createElement('div')
+containerBtn.setAttribute('class','containerBtn')
+containerSup.appendChild(containerBtn)
 
-const createGrid = document.createElement('button')
-createGrid.setAttribute('id','createGrid')
-createGrid.textContent = 'createGrid'
-divSup.appendChild(createGrid)
+const createBtn = document.createElement('button')
+createBtn.setAttribute('id','createBtn')
+createBtn.textContent = 'Create Grid'
+containerBtn.appendChild(createBtn)
 
 const resetGrid = document.createElement('button')
 resetGrid.setAttribute('id','resetGrid')
-resetGrid.textContent = 'resetGrid'
-divSup.appendChild(resetGrid)
+resetGrid.textContent = 'Reset Grid'
+containerBtn.appendChild(resetGrid)
 
-const resizeGrid = document.createElement('button')
-resizeGrid.setAttribute('id','resizeGrid')
-resizeGrid.textContent = 'resizeGrid'
-divSup.appendChild(resizeGrid)
+const resetColor = document.createElement('button')
+resetColor.setAttribute('id','resetColor')
+resetColor.textContent = 'Reset Color'
+containerBtn.appendChild(resetColor)
 
-resizeGrid.addEventListener('click', () => {
-    const newSize = prompt('NEW SIZE?')
-    resizerGrid(newSize*newSize)
+createBtn.addEventListener('click', () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    requestGrid()
 })
 
-function resizerGrid(newSize){
-    let limitDivs = 0
-    while (limitDivs<newSize){
-        const div = document.createElement('div')
-        div.classList.add("div-item");
-        main.appendChild(div)
-        limitDivs++
-        if (limitDivs == newSize) {
-            break
-        }
-        div.addEventListener('mouseenter', () => {
-            div.style.backgroundColor = randomColor()  
-        })
-        resetGrid.addEventListener('click', () => {
-            div.style.backgroundColor = 'white'  
+resetGrid.addEventListener('click', () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+})
 
-        })
+function createGrid(rows, columns) {
+    container.style.setProperty('--rows', rows);
+    container.style.setProperty('--columns', columns);
+  
+    for (let i = 1; i <= rows * columns; i++) {
+      const item = document.createElement('div');
+      item.className = 'item';
+      item.addEventListener('mouseenter', () => {
+        item.style.backgroundColor = randomColor()  
+      })
+      resetColor.addEventListener('click', () => {
+        item.style.backgroundColor = 'aqua'  
+      })
 
+      container.appendChild(item);
     }
+}
+  
+function requestGrid() {
+    const size = prompt("Enter a number between 1 and 100.")
+    if (size <= 100) {
+        createGrid(size,size)
+      } else {
+        alert("The number is incorrect; it must be between 1 and 100.")
+        requestSize();
+      }
     
 }
-
-createGrid.addEventListener('click',() => {
-    let limitDivs = 0
-    while (limitDivs<16){
-        const div = document.createElement('div')
-        div.classList.add("div-item");
-        main.appendChild(div)
-        limitDivs++
-        if (limitDivs == 16) {
-            break
-        }
-        div.addEventListener('mouseenter', () => {
-            div.style.backgroundColor = randomColor()  
-        })
-        resetGrid.addEventListener('click', () => {
-            div.style.backgroundColor = 'white'  
-
-        })
-
-    }
-})
-
-
-
 
 function randomColor(){
-    var firstNum = Math.floor(Math.random() * 256);
-    var secondNum = Math.floor(Math.random() * 256);   
-    var thirdNum = Math.floor(Math.random() * 256);   
-    
-    let color = `rgb(${firstNum}, ${secondNum}, ${thirdNum})`
-    return color;
+  var firstNum = Math.floor(Math.random() * 256);
+  var secondNum = Math.floor(Math.random() * 256);   
+  var thirdNum = Math.floor(Math.random() * 256);   
+  
+  let color = `rgb(${firstNum}, ${secondNum}, ${thirdNum})`
+  return color;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const footer = document.createElement('footer')
+footer.setAttribute('class', 'footer')
+footer.textContent = 'Copyright © Diego Vallejos for The Odin Project 2024'
+body.appendChild(footer)
